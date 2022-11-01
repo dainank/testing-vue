a26a4d145fbbec1401ae56f359cedd56784d35f3
dainank <dainank@proton.me>
2022-11-01 12:17:21 +0100

    breaking test

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="textContent">
     item
   </div>
 </template>
--- src/components/__tests__/Item.spec.js
+++ src/components/__tests__/Item.spec.js
@@ -1,7 +1,10 @@
 import Item from '../Item.vue'
+import Vue from 'vue'
 
-describe('Item.vue', () => {
-  test('sanity test', () => {
-    console.log(Item)
+describe('Item.vue', () => { // grp tests
+  test('renders "item"', () => { // test name & function to test
+    const Ctor = Vue.extend(Item) // create Vue constructor
+    const vm = new Ctor().$mount() // create instance
+    expect(vm.$el.textContent).toContain('item')
   })
 })
